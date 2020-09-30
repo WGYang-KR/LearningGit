dc0178eb991f4a196e3913478ca6840bb76115c1
WGYang-KR <wollxxllow@gmail.com>
2020-09-30 16:04:14 +0900

    multi-table.sh 추가

---
+++ multi-table.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+echo "Para: $1 $2"
+i=1
+
+while [ $i -le $1 ]
+do
+	j=1
+	while [ $j -le $2 ]
+	do
+	echo -n "$i*$j=`expr $i \* $j` "
+	j=`expr $j + 1`
+	done
+
+echo -n "\n"
+i=`expr $i + 1`
+done
+
+exit 0
